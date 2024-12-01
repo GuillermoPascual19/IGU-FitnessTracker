940d73eee28ce98d7b3b897404e94fba16e0237c
Guillermo <119169941+GuillermoPascual19@users.noreply.github.com>
2024-12-02 00:01:16 +0100

    Add files via upload

--- GPM_IGU_PracticaFinal/GPM_IGU_PracticaFinal/SecondaryExecutions.xaml.cs
+++ GPM_IGU_PracticaFinal/GPM_IGU_PracticaFinal/SecondaryExecutions.xaml.cs
@@ -21,19 +21,19 @@ namespace GPM_IGU_PracticaFinal
     /// </summary>
     public partial class SecondaryExecutions : Window
     {
-        ObservableCollection<Exercises> listExercises = new ObservableCollection<Exercises>();
+        Exercises exercise;
         Executions exec;
-        public SecondaryExecutions(ObservableCollection<Exercises> exercises)
+        public SecondaryExecutions(Exercises exercises)
         {
             InitializeComponent();
-            listExercises = exercises;
-            //TableExecutions.ItemsSource = listExercises.ListExecution;
+            exercise = exercises;
+            TableExecutions.ItemsSource = exercise.ListExecution;
 
         }
 
         private void Add_Button_Click(object sender, RoutedEventArgs e)
         {
-            Executions ex; 
+            
 
         }
 
@@ -49,7 +49,78 @@ namespace GPM_IGU_PracticaFinal
 
         private void Add_4Exec_Button_Click(object sender, RoutedEventArgs e)
         {
+            Executions ex;
 
+            int reps = 0, weight = 0;
+            Random rand = new Random();
+            DateTime date = new DateTime(1999, 1, 1);
+            int range;
+
+            //=======================First Exercise========================
+            reps = rand.Next(1, 30);
+            weight = rand.Next(1, 200);
+            range = (DateTime.Today - date).Days;
+            date = date.AddDays(rand.Next(range));
+            ex = new Executions(reps, weight, date);
+            exercise.ListExecution.Add(ex);
+
+            //=======================Second Exercise=======================
+            reps = rand.Next(1, 30);
+            weight = rand.Next(1, 200);
+            range = (DateTime.Today - date).Days;
+            date = date.AddDays(rand.Next(range));
+            ex = new Executions(reps, weight, date);
+            exercise.ListExecution.Add(ex);
+
+            //=======================Third Exercise========================
+            reps = rand.Next(1, 30);
+            weight = rand.Next(1, 200);
+            range = (DateTime.Today - date).Days;
+            date = date.AddDays(rand.Next(range));
+            ex = new Executions(reps, weight, date);
+            exercise.ListExecution.Add(ex);
+
+            //=======================Fourth Exercise=======================
+            reps = rand.Next(1, 30);
+            weight = rand.Next(1, 200);
+            range = (DateTime.Today - date).Days;
+            date = date.AddDays(rand.Next(range));
+            ex = new Executions(reps, weight, date);
+            exercise.ListExecution.Add(ex);
+
+            exercise.ReorderDate();
+            TableExecutions.ItemsSource = exercise.ListExecution;
+        }
+
+        private void DrawAxis()
+        {
+            Line ejeX;
+            Line ejeY;
+            double width = 0;
+            double height = 0;
+
+            width = graphicCanvas.ActualWidth;
+            height = graphicCanvas.ActualHeight;
+
+            graphicCanvas.Children.Clear();
+
+            ejeX = new Line();
+            ejeX.Stroke = Brushes.Black;
+            ejeX.StrokeThickness = 2;
+            ejeX.X1 = 0;
+            ejeX.Y1 = 20;
+            ejeX.X2 = width - 20;
+            ejeX.Y2 = 20;
+            graphicCanvas.Children.Add(ejeX);
+
+            ejeY = new Line();
+            ejeY.Stroke = Brushes.Black;
+            ejeY.StrokeThickness = 2;
+            ejeY.X1 = 20;
+            ejeY.Y1 = 0;
+            ejeY.X2 = 20;
+            ejeY.Y2 = height - 20;
+            graphicCanvas.Children.Add(ejeY);
         }
     }
 }
